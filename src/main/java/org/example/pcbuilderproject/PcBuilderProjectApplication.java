6c10a10eb984f93e8676a931c8b5d3c4054c1af0
Dominik <dominik.crnjak@gmail.com>
2024-10-22 13:43:28 +0200

    first commit

---
+++ src/main/java/org/example/pcbuilderproject/PcBuilderProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.pcbuilderproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcBuilderProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcBuilderProjectApplication.class, args);
+    }
+
+}
